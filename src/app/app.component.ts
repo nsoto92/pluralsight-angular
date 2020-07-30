722a512bd0f6a48ed09a8030e3b4d318cf39826a
nsoto92 <nor.soto92@gmail.com>
2020-07-30 14:36:53 -0700

    Mod 4 finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'pm-root',      //Product management root
-  template: `
+@Component({            //Component decorator defines Metadata for component, prefix with @ and suffix with ()
+  selector: 'pm-root',    // Selector property defines name of component to be used in HTML, not necessary if component not used in HTM    
+  template: // Template defines view for HTML
+    `         
 <div><h1>{{pageTitle}}</h1>
   <div>My First Component</div>
 </div>
 `
 })
-export class AppComponent {
-  pageTitle: string = 'Wezt Product Management'
+export class AppComponent {               //use PascalCasing for classes and append Component to class name, include export key word
+  pageTitle: string = 'Wezt Product Management'    //camelCase porperty name, include  data type and default value
 }
