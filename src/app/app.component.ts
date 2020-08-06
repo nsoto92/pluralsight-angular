8fe6cb3ebd6e0b39004987780b8c4554345ae9d9
nsoto92 <nor.soto92@gmail.com>
2020-08-06 16:34:34 -0700

    product component built

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   template: // Template defines view for HTML
     `         
 <div><h1>{{pageTitle}}</h1>
-  <div>My First Component</div>
+  <pm-products></pm-products>
 </div>
 `
 })
